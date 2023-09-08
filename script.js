0c87ceec55118618ba2ebfa87ab6a8b10c432432
habiblimon <md.habiblimon94@gmail.com>
2023-09-09 00:15:44 +0600

    JavaScript 101 assignment update

---
+++ script.js
@@ -0,0 +1,34 @@
+//Task 1: Hello World with Variables
+console.log("Hello");
+
+var greeting = "Hello!";
+console.log(greeting);
+console.log(typeof greeting);
+
+
+var name = " Habib Limon";
+
+console.log(greeting+" "+name);
+
+var all =greeting+name;
+console.log(all);
+console.log(typeof all);
+
+
+//Task 2: Basic Math Operations
+
+var num1 = 4;
+var num2 = 6;
+var total = num1+num2;
+
+console.log(num1+num2);
+console.log(total);
+console.log(typeof total);
+
+console.log(num1+num2); //addition
+console.log(num2-num1); //subtraction
+console.log(num1*num2); //multiplication
+console.log(num1%num2); //modulus 
+
+
+
