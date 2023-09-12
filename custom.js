997ab206678aa0dc0a9c2e26e6b5a3ada1e842cd
habiblimon <md.habiblimon94@gmail.com>
2023-09-12 23:55:22 +0600

    added two more file update

---
+++ custom.js
@@ -0,0 +1,79 @@
+//class 01 JavaScrip Basicsc
+
+console.log("*** class 01 ***");
+
+ console.log("Hellow Javascript"); //string - 
+
+ console.log(10+30); //number 
+
+
+ //variable - box 
+
+ var patro = "banana";
+ var num = 10;
+ console.log(patro);
+ 
+
+
+ //typof 
+ console.log(typeof patro);
+ console.log(typeof num);
+ console.log(typeof (patro+num));// if string and number adding togather it will be show string typeof.
+
+
+
+ //operation  (+ - * /)
+
+ console.log(typeof (10/10));//division
+ console.log(typeof (10*2));//multiply
+ console.log(typeof (30-10));//subtract
+ console.log(typeof (10+10)); //addition
+
+ //first we should define a variable with var
+ //fist letter should be smaller
+ //cameCase/underscore 
+ //Name should be meaningful (avoid using , x, y, Z, a, b etc)
+
+var numOne = 10; //camelCase
+var num_Tow = 30; //under_score
+
+
+var addition = numOne+num_Tow;
+console.log("addition :", addition);
+
+
+//to the power 
+console.log(3 ** 3); // 3 x 3 x 3 = 27 
+
+//Modulus
+
+console.log(10 % 3); //10 / 3 = 3 ,  "(1)" reminder number.
+
+//increment /decrement 
+
+var numInc = 10;
+numInc ++;
+
+console.log("increment (++)", numInc);
+
+var numDec = 10;
+numDec --;
+numDec --; // result should be show = 8
+
+console.log("Decriment (--)", numDec);
+
+//true or false
+
+var isHot = true;
+var isCold = false;
+
+console.log(typeof isHot); //boolean
+
+
+//undefind, null
+var counter;
+
+console.log(counter) // undefind 
+
+
+
