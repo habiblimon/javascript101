50eccda53dc03705472ed8d302e72dbcf654c3bd
habiblimon <md.habiblimon94@gmail.com>
2023-10-07 01:59:29 +0600

    string file update

--- string.js
+++ string.js
@@ -12,16 +12,20 @@ console.log("length of sentance :", sentence.length);
 
 //Make a new string (add two string) - concat
 let greeting2 = "good night";
-let newGreeting = greeting2.concat(", abdullah");
+let newGreeting = greeting2.concat(", abdullah"); // concat with variable value .
 
 let firstName = "Habib";
 let lastName = "limon";
 
-console.log(firstName+" "+lastName);
+console.log(firstName +" "+ lastName);
 console.log(newGreeting);
 
+//subString - take a portion of string .
+let newSentence1 = sentence.substring(0, 9);
+console.log("subString :", newSentence1);
+
 //slice - take a portion of a string (it takes negative value as well)
-let slice = sentence.slice(-20, -3);
+let slice = sentence.slice(-20, -3); //
 console.log(slice);
 
 let blogPost = 
@@ -29,7 +33,7 @@ let blogPost =
 
 console.log(blogPost.slice(0, 25)+"......."); 
 
-//charAt ->(search a letter from a string )
+//charAt (index) ->(search a letter from a string )
 
 let word = "Coffee";
 let serchLetter = word.charAt(5);// /e
@@ -37,7 +41,7 @@ console.log(serchLetter);
 
 //indexof (string) //-1 -> not found
 let newSentence = "I love coding";
-let searchWord = newSentence.indexOf("love");
+let searchWord = newSentence.indexOf("coding");
 
 console.log(searchWord);
 
@@ -57,13 +61,32 @@ console.log(upperCase.toUpperCase());
 
 //trim -> removes space from a string 
 let untidy = "    how are you";
-console.log(untidy.trim());
+console.log(untidy.trim()); // remove / cut start & end space .
 
-//find if the word "hello" is present in the 
+
+//find if the word "hello" is present in the sentence !
 let givenSentence = "hello, how is bob doing today!";
-let lowerCaseGivenSentence = givenSentence.toLocaleLowerCase();
-let searchFor ="hellow";
-let searchForTowlowercase = searchFor.toLocaleLowerCase();
+let lowerCaseGivenSentence = givenSentence.toLowerCase();
+let searchFor ="hello";
+let searchForTowlowercase = searchFor.toLowerCase();
+
+if(lowerCaseGivenSentence.includes(searchForTowlowercase)){
+    console.log(searchFor, "is found!");
+}else{
+    console.log("opps!....", searchFor, "is not found");
+}
+
+
+
+//one line code .
+
+if(givenSentence.toLowerCase().includes(searchFor.toLowerCase())){
+    console.log("Found.....");
+}else{
+    console.log("Not Found!");
+}
+
+
 
 
 
