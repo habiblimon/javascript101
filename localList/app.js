354ebf49b9c43be62c60b5285d440fd1b70bda1e
habiblimon <md.habiblimon94@gmail.com>
2023-10-26 00:13:28 +0600

    local storage

--- localList/app.js
+++ localList/app.js
@@ -1,99 +1,80 @@
-//selectors 
+// selectors
 const itemForm = document.getElementById("item-form");
 const itemInput = document.getElementById("item-input");
 const itemList = document.getElementById("items-list");
 const itemTable = document.getElementById("item-table");
 const clearBtn = document.getElementById("clear-btn");
 const listContainer = document.getElementById("list-container");
-
 const filter = document.getElementById("filter");
+// Add Item
+// Remove Item
+// Filter Item
+// Clear All Item
+let count = 1;
+function addItem(e) {
+  // to stop browser reload
+  e.preventDefault();
 
+  const newItem = itemInput.value;
 
+  if (newItem === "") {
+    alert("Please add an item!");
+    return;
+  }
+  itemTable.innerHTML += `
+    <tr class="item">
+        <th scope="row">${count}</th>
+        <td class="item-name">${newItem}</td>
+        <td><button class="btn btn-danger remove-item"><i class="bi bi-trash3"></i></button></td>
+    </tr>
+  `;
 
-//Add item
-//Remove item
-//filter item
-//clear All item
+  itemInput.value = "";
+  checkInterface();
+  count++;
+}
 
+function removeItem(e) {
+  if (e.target.parentElement.classList.contains("remove-item")) {
+    e.target.parentElement.parentElement.parentElement.remove();
+  }
+  checkInterface();
+}
 
-let count =1;
-function addItem(e){
-    //To stop brower reload.
-    e.preventDefault();
+function filterItem(e) {
+  const items = document.querySelectorAll(".item");
+  const searchText = e.target.value.toLowerCase();
 
-    const newItem = itemInput.value;
+  items.forEach((item) => {
+    const itemName = item.children[1].textContent.toLowerCase();
 
-    if(newItem ===""){
-        alert("please add an item");
-        return;
+    if (itemName.indexOf(searchText) !== -1) {
+      item.style.display = "";
+    } else {
+      item.style.display = "none";
     }
-
-   
-    itemTable.innerHTML +=`
-        <tr class="item">
-            <th scope="row">${count}</th>
-            <td class="item-name">${newItem}</td>
-            <td><button class="btn btn-danger remove-item"><i class="bi bi-trash3"></i></button></td>
-        </tr>
-    `;
-
-    //empty input data after posted input. 
-    itemInput.value="";
-    checkInterface();
-    count++;
-
+  });
 }
 
-function removeItem(e){
-    if(e.target.parentElement.classList.contains("remove-item")){
-        e.target.parentElement.parentElement.parentElement.remove();
-    }
-    checkInterface();
-   
+function clearList() {
+  itemTable.innerHTML = "";
 }
 
-function filterItems(e){
-    const items = document.querySelectorAll(".item");
-    const searchText = e.target.value.toLocaleLowerCase();
-    
-    items.forEach((item) =>{
-        const itemName = item.children[1].textContent.toLocaleLowerCase();
-       
-        if(itemName.indexOf(searchText) !== -1){
-            item.style.display ="";
-        }else{
-            item.style.display="none";
-        }
+function checkInterface() {
+  const items = document.querySelectorAll(".item");
 
-    });
+  if (items.length === 0) {
+    listContainer.style.display = "none";
+  } else {
+    console.log(items);
+    listContainer.style.display = "block";
+  }
 }
 
-
-function clearList(){
-    itemTable.innerHTML = "";
-}
-
-
-function checkInterface(){
-    const items = document.querySelectorAll(".item");
-
-    if(items.length === 0){
-       listContainer.style.display = "none";
-       
-    }else{
-        listContainer.style.display = "block";
-    }
-}
-
-
-
-
-
-
-//Event Listen
+// Event Listeners
 itemForm.addEventListener("submit", addItem);
 itemTable.addEventListener("click", removeItem);
 clearBtn.addEventListener("click", clearList);
-filter.addEventListener("input", filterItems);
+filter.addEventListener("input", filterItem);
 
 checkInterface();
